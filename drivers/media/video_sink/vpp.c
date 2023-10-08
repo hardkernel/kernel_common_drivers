fddb0aab1f3f16d8f42cf2c98ada00c4fa07da43
Pengcheng Chen <pengcheng.chen@amlogic.com>
2023-10-08 11:08:17 +0800

    vpp: force h crop 4 aligned when 2slice and vd1s1_vd2_prebld_en [1/1]
    PD#SWPL-141710
    
    Problem:
    osd can't display with h crop not 4 aligned and vsize is odd
    when 2slice and vd1s1_vd2_prebld_en
    
    Solution:
    force h crop 4 aligned when 2slice and vd1s1_vd2_prebld_en
    
    Verify:
    s5
    
    Change-Id: Ie8da4b07afdf82a20e60976c15789593ccb76e11
    Signed-off-by: Pengcheng Chen <pengcheng.chen@amlogic.com>

--- drivers/media/video_sink/vpp.c
+++ drivers/media/video_sink/vpp.c
@@ -1549,6 +1549,7 @@ static int vpp_set_filters_internal
 	bool src_crop_adjust = false;
 	bool force_dw = false;
 	u32 force_skip_cnt = 0, slice_num = 0;
+	bool vd1s1_vd2_prebld_en = false;
 
 	if (!input)
 		return vppfilter_fail;
@@ -1670,11 +1671,12 @@ RESTART_ALL:
 	crop_bottom = video_source_crop_bottom / crop_ratio;
 
 	slice_num = get_slice_num(input->layer_id);
-	if (slice_num == 2) {
+	vd1s1_vd2_prebld_en = get_vd1s1_vd2_prebld_en(input->layer_id);
+	if (slice_num == 2  && !vd1s1_vd2_prebld_en) {
 		/* crop left must 2 aligned */
 		crop_left = (crop_left + 1) & ~0x01;
 		crop_right = (crop_right + 1) & ~0x01;
-	} else if (slice_num == 4) {
+	} else if (slice_num == 4  || vd1s1_vd2_prebld_en) {
 		/* crop left must 4 aligned */
 		crop_left = (crop_left + 3) & ~0x03;
 		crop_right = (crop_right + 3) & ~0x03;
