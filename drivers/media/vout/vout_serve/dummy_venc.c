74e8529e536fac753981f3bc496fe9b851327627
Ao Xu <ao.xu@amlogic.com>
2023-10-19 16:35:20 +0800

    hdmitx: remove dummyl suspend/resume and vout_device legacy function [1/1]
    PD#SWPL-141589
    PD#SWPL-141801
    
    Problem:
    get_attr, setup_attr, video_mute in vout_device is not needed
    drm suspend/resume do the same thing with dummyl suspend/resume
    
    Solution:
    remove vout_device legacy interface
    remove dummyl suspend/resume interface
    
    Verify:
    ohm
    
    Test:
    DRM-TX-19
    
    Change-Id: I950acd61ff2742e452f15221c2a542f6aafecdc7
    Signed-off-by: Ao Xu <ao.xu@amlogic.com>

--- drivers/media/vout/vout_serve/dummy_venc.c
+++ drivers/media/vout/vout_serve/dummy_venc.c
@@ -1385,22 +1385,6 @@ static int dummy_encl_disable(enum vmode_e cur_vmod, void *data)
 	return 0;
 }
 
-#ifdef CONFIG_PM
-static int dummy_encl_suspend(void *data)
-{
-	dummy_encl_disable(VMODE_DUMMY_ENCL, data);
-	VOUTPR("%s finished\n", __func__);
-	return 0;
-}
-
-static int dummy_encl_resume(void *data)
-{
-	dummy_encl_set_current_vmode(VMODE_DUMMY_ENCL, data);
-	VOUTPR("%s finished\n", __func__);
-	return 0;
-}
-#endif
-
 static int dummy_encl_vout_state;
 static int dummy_encl_vout_set_state(int bit, void *data)
 {
@@ -2317,8 +2301,6 @@ static int dummy_venc_resume(struct platform_device *pdev)
 		dummy_encp_resume((void *)dummy_encp_drv);
 	if (dummy_enci_drv && dummy_enci_drv->vout_valid)
 		dummy_enci_resume((void *)dummy_enci_drv);
-	if (dummy_encl_drv && dummy_encl_drv->vout_valid)
-		dummy_encl_resume((void *)dummy_encl_drv);
 
 	return 0;
 }
@@ -2329,8 +2311,6 @@ static int dummy_venc_suspend(struct platform_device *pdev, pm_message_t state)
 		dummy_encp_suspend((void *)dummy_encp_drv);
 	if (dummy_enci_drv && dummy_enci_drv->status)
 		dummy_enci_suspend((void *)dummy_enci_drv);
-	if (dummy_encl_drv && dummy_encl_drv->status)
-		dummy_encl_suspend((void *)dummy_encl_drv);
 
 	return 0;
 }
