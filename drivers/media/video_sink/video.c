972e64b4b4e417807e9bbde778825e3f31c38a2b
hai.cao <hai.cao@amlogic.com>
2023-12-11 14:38:11 +0800

    video: debug vsr for s7d [1/1]
    PD#SWPL-149452
    
    Problem:
    s7d video bringup
    
    Solution:
    complete it
    
    Verify:
    ptm
    
    Change-Id: Ic59ed825b97d030949a5e85f060b1f2218d61c60
    Signed-off-by: hai.cao <hai.cao@amlogic.com>

--- drivers/media/video_sink/video.c
+++ drivers/media/video_sink/video.c
@@ -13347,6 +13347,7 @@ static struct class_attribute amvideo_class_attrs[] = {
 		0664,
 		vsr_top_en_show,
 		vsr_top_en_store),
+
 };
 
 static struct class_attribute amvideo_poll_class_attrs[] = {
