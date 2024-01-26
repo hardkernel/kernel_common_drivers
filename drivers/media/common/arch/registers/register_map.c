4ed34b91791d854bae32785c317df8662da90a10
yuhua.lin <yuhua.lin@amlogic.com>
2024-01-26 20:06:48 +0800

    s6: add codec_io/canvas/rdma support for s6 [1/1]
    PD#SWPL-156543
    
    Problem:
    s6 codec_io/canvas/rdma bringup
    
    Solution:
    complete it
    
    Verify:
    pxp
    
    Change-Id: Ic9facdff23c43b330d8203744d9ca6a3a0172b8f
    Signed-off-by: yuhua.lin <yuhua.lin@amlogic.com>

--- drivers/media/common/arch/registers/register_map.c
+++ drivers/media/common/arch/registers/register_map.c
@@ -156,6 +156,9 @@ static struct codecio_device_data_s codecio_s7d = {
 	.cpu_id = MESON_CPU_MAJOR_ID_S7D,
 };
 
+static struct codecio_device_data_s codecio_s6 = {
+	.cpu_id = MESON_CPU_MAJOR_ID_S6,
+};
 #endif
 #endif
 
@@ -298,6 +301,10 @@ static const struct of_device_id codec_io_dt_match[] = {
 	{
 		.compatible = "amlogic, meson-s7d, codec-io",
 		.data = &codecio_s7d,
+	},
+	{
+		.compatible = "amlogic, meson-s6, codec-io",
+		.data = &codecio_s6,
 	},
 #endif
 #endif
