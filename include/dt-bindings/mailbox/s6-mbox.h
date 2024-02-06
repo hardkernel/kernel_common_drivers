a078845dfb1b83d4c571ebb65d2f4d80da4b0f9e
Yao Jie <jie.yao@amlogic.com>
2024-02-06 11:01:33 +0800

    mailbox: s6 mailbox pxp bringup [1/1]
    PD#SWPL-156519
    
    Problem:
    S6 mailbox pxp bringup
    
    Solution:
    S6 mailbox pxp bringup
    
    Verify:
    PXP
    
    Change-Id: I08917759de24df334397db8f49f64b057172f3f3
    Signed-off-by: Yao Jie <jie.yao@amlogic.com>

--- include/dt-bindings/mailbox/s6-mbox.h
+++ include/dt-bindings/mailbox/s6-mbox.h
@@ -3,31 +3,40 @@
  * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
  */
 
-#ifndef __S7D_MBOX_H__
-#define __S7D_MBOX_H__
+#ifndef __S6_MBOX_H__
+#define __S6_MBOX_H__
 
 #include "amlogic,mbox.h"
 
 // MBOX DRIVER ID
-#define S7D_AO2REE        0
-#define S7D_REE2AO0       (S7D_AO2REE + 1)
-#define S7D_REE2AO1       (S7D_AO2REE + 2)
-#define S7D_REE2AO2       (S7D_AO2REE + 3)
-#define S7D_REE2AO3       (S7D_AO2REE + 4)
-#define S7D_REE2AO4       (S7D_AO2REE + 5)
-#define S7D_REE2AO5       (S7D_AO2REE + 6)
+#define S6_DSPA2REE0     0
+#define S6_REE2DSPA0     1
+#define S6_REE2DSPA1     2
+#define S6_REE2DSPA2     3
+#define S6_AO2REE        4
+#define S6_REE2AO0       (S6_AO2REE + 1)
+#define S6_REE2AO1       (S6_AO2REE + 2)
+#define S6_REE2AO2       (S6_AO2REE + 3)
+#define S6_REE2AO3       (S6_AO2REE + 4)
+#define S6_REE2AO4       (S6_AO2REE + 5)
+#define S6_REE2AO5       (S6_AO2REE + 6)
 
 // DEVICE TREE ID
-#define S7D_REE2AO_DEV    S7D_REE2AO0
-#define S7D_REE2AO_VRTC   S7D_REE2AO1
-#define S7D_REE2AO_KEYPAD S7D_REE2AO2
-#define S7D_REE2AO_AOCEC  S7D_REE2AO3
-#define S7D_REE2AO_ETH    S7D_REE2AO4
-#define S7D_REE2AO_LED    S7D_REE2AO5
+#define S6_REE2DSPA_DEV  S6_REE2DSPA0
+#define S6_REE2DSPA_DSP  S6_REE2DSPA1
+#define S6_DSPA2REE_DEV  S6_DSPA2REE0
+#define S6_REE2AO_DEV    S6_REE2AO0
+#define S6_REE2AO_VRTC   S6_REE2AO1
+#define S6_REE2AO_KEYPAD S6_REE2AO2
+#define S6_REE2AO_AOCEC  S6_REE2AO3
+#define S6_REE2AO_ETH    S6_REE2AO4
+#define S6_REE2AO_LED    S6_REE2AO5
 
 // MBOX CHANNEL ID
-#define S7D_MBOX_AO2REE    2
-#define S7D_MBOX_REE2AO    3
-#define S7D_MBOX_NUMS      2
+#define S6_MBOX_DSPA2REE  0
+#define S6_MBOX_REE2DSPA  1
+#define S6_MBOX_AO2REE    2
+#define S6_MBOX_REE2AO    3
+#define S6_MBOX_NUMS      4
 
-#endif /* __S7D_MBOX_H__ */
+#endif /* __S6_MBOX_H__ */
