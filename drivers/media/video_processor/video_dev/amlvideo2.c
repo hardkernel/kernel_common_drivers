8e88009f249724d9dce07c50abe2827a5da82957
chen.xu <chen.xu@amlogic.com>
2024-02-26 17:29:26 +0800

    amlvideo2: optimize pagetrace create_ge2d_work_queue [1/1]
    PD#SWPL-158350
    
    Problem:
    amlvideo2 need optimize pagetrace create_ge2d_work_queue.
    
    Solution:
    modify create_ge2d_work_queue in thread start.
    
    Verify:
    TXHD2
    
    Change-Id: I625f94f5cd4d88aa3c390c8a0b9ea91bc2f7748f
    Signed-off-by: chen.xu <chen.xu@amlogic.com>

--- drivers/media/video_processor/video_dev/amlvideo2.c
+++ drivers/media/video_processor/video_dev/amlvideo2.c
@@ -4558,6 +4558,12 @@ static int amlvideo2_start_thread(struct amlvideo2_fh *fh)
 	node->tmp_vf = NULL;
 	dma_q->task_running = 1;
 
+	node->context = create_ge2d_work_queue();
+	if (!node->context) {
+		mutex_unlock(&node->mutex);
+		pr_info("amlvideo2 create_ge2d_work_queue error!\n");
+		return -1;
+	}
 	#ifdef MULTI_NODE
 	dma_q->kthread =
 		kthread_run(amlvideo2_thread, fh,
@@ -4623,6 +4629,8 @@ static void amlvideo2_stop_thread(struct amlvideo2_node_dmaqueue *dma_q)
 
 		dma_q->kthread = NULL;
 	}
+	if (node->context)
+		destroy_ge2d_work_queue(node->context);
 	mutex_unlock(&node->mutex);
 	if (amlvideo2_dbg_en & 1) {
 		if (node->vid == 0)
@@ -6760,9 +6768,6 @@ static int amlvideo2_release_node(struct amlvideo2_device *vid_dev)
 			vfd = vid_dev->node[i]->vfd;
 			video_device_release(vfd);
 			vf_unreg_receiver(&vid_dev->node[i]->recv);
-			if (vid_dev->node[i]->context)
-				destroy_ge2d_work_queue
-					(vid_dev->node[i]->context);
 			kfree(vid_dev->node[i]->fh);
 			kfree(vid_dev->node[i]->amlvideo2_pool_ready);
 			vid_dev->node[i]->fh = NULL;
@@ -6835,12 +6840,7 @@ static int amlvideo2_create_node(struct platform_device *pdev, int node_id)
 			 (struct vframe_s **)
 			 &vid_node->amlvideo2_pool_ready[0]);
 	}
-	vid_node->context = create_ge2d_work_queue();
-	if (!vid_node->context) {
-		kfree(vid_node->amlvideo2_pool_ready);
-		kfree(vid_node);
-		return ret;
-	}
+
 	/* init video dma queues */
 	INIT_LIST_HEAD(&vid_node->vidq.active);
 	init_waitqueue_head(&vid_node->vidq.wq);
@@ -6855,7 +6855,6 @@ static int amlvideo2_create_node(struct platform_device *pdev, int node_id)
 #endif
 	vfd = video_device_alloc();
 	if (!vfd) {
-		destroy_ge2d_work_queue(vid_node->context);
 		kfree(vid_node->amlvideo2_pool_ready);
 		kfree(vid_node);
 		return ret;
@@ -6870,7 +6869,6 @@ static int amlvideo2_create_node(struct platform_device *pdev, int node_id)
 	if (ret < 0) {
 		ret = -ENODEV;
 		video_device_release(vfd);
-		destroy_ge2d_work_queue(vid_node->context);
 		kfree(vid_node->amlvideo2_pool_ready);
 		kfree(vid_node);
 		return ret;
@@ -6879,7 +6877,6 @@ static int amlvideo2_create_node(struct platform_device *pdev, int node_id)
 	fh = kzalloc(sizeof(*fh), GFP_KERNEL);
 	if (!fh) {
 		video_device_release(vfd);
-		destroy_ge2d_work_queue(vid_node->context);
 		kfree(vid_node->amlvideo2_pool_ready);
 		kfree(vid_node);
 		return ret;
