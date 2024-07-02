5552cfd0dd8f642576457e7843d7ea5d599aad50
saisai.chang <saisai.chang@amlogic.com>
2024-07-02 10:05:18 +0800

    audio: start pdm clk enable [1/1]
    PD#SWPL-172596
    
    Problem:
    1.pdm clk will close while arm sleep
    2.sc2 not need vad driver
    
    Solution:
    1.keep pdm clk opening
    2.disabled vad
    
    Verify:
    ohm
    
    Change-Id: Iae661918a4a01fbe38dcc9772a5b528633d14e68
    Signed-off-by: saisai.chang <saisai.chang@amlogic.com>

--- sound/soc/amlogic/auge/pdm.h
+++ sound/soc/amlogic/auge/pdm.h
@@ -117,6 +117,7 @@ struct aml_pdm {
 	struct regulator *regulator_vcc3v3;
 	struct regulator *regulator_vcc5v;
 	bool earc_use_48k;
+	int start_clk_enable;
 };
 
 int pdm_get_train_sample_count_from_dts(void);
