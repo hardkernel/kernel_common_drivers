5ca85e4ff4000f103858f24ccac0806e7b366370
Zhuo Wang <zhuo.wang@amlogic.com>
2024-08-15 10:00:12 +0800

    eth: update t6d analog setting [1/1]
    PD#SWPL-182020
    
    Problem:
    update t6d analog setting
    
    Solution:
    update t6d analog setting
    
    Verify:
    BR301-T950D5#53
    
    Change-Id: Ie1483a00fda546a916eb271b1840335bf889f7a9
    Signed-off-by: Zhuo Wang <zhuo.wang@amlogic.com>

--- drivers/net/phy/meson-gxl.c
+++ drivers/net/phy/meson-gxl.c
@@ -336,7 +336,6 @@ static int custom_internal_config(struct phy_device *phydev)
 		/*env not set, efuse not valid return*/
 		pr_info("env not set, efuse also invalid\n");
 	}
-
 	/*voltage phy*/
 	/*t3x*/
 	if (voltage_phy == 1) {
@@ -375,6 +374,17 @@ static int custom_internal_config(struct phy_device *phydev)
 	/*s7d s6*/
 	if (voltage_phy == 5 || voltage_phy == 6) {
 		phy_tst_write(phydev, 0x16, 0x8402);
+		phy_tst_write(phydev, 0x15, 0x4408);
+		pr_debug("setup voltage phy reg16 %x reg15 %x\n",
+				phy_tst_read(phydev, 0x16),
+				phy_tst_read(phydev, 0x15));
+		phy_tst_write(phydev, 0x1b, 0x40a0);
+		usleep_range(800, 1000);
+		phy_tst_write(phydev, 0x1b, 0x00a0);
+	}
+	/*t6d*/
+	if (voltage_phy == 7) {
+		phy_tst_write(phydev, 0x16, 0x8406);
 		phy_tst_write(phydev, 0x15, 0x4408);
 		pr_debug("setup voltage phy reg16 %x reg15 %x\n",
 				phy_tst_read(phydev, 0x16),
