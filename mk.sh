a83ddd8bf5046c843d1ba45f55e7fd9475501370
qinglin.li <qinglin.li@amlogic.com>
2024-09-06 19:16:36 +0800

    module: build ext module without bazel [1/2]
    PD#OTT-65725
    
    Problem:
    build ext module without bazel
    
    Solution:
    use bazel-common14-5.15 as kernel output source
    add build_ext_module_without_bazel function
    
    Verify:
    local
    
    Change-Id: I5ac02141b21ac7d9b223bfc7870a8b140bca6d46
    Signed-off-by: qinglin.li <qinglin.li@amlogic.com>

--- mk.sh
+++ mk.sh
@@ -244,6 +244,7 @@ if [[ "${FULL_KERNEL_VERSION}" != "common13-5.15" && "${ARCH}" = "arm64" && ${BA
 	source ${KERNEL_DIR}/${COMMON_DRIVERS_DIR}/amlogic_utils.sh
 
 	bazel_extra_cmds
+	build_ext_module_without_bazel
 	if [[ -n ${COPY_DEV_CONFIGS} ]]; then
 		for config_name in ${COPY_DEV_CONFIGS}; do
 			if [[ -f ${ROOT_DIR}/${KERNEL_DIR}/${COMMON_DRIVERS_DIR}/arch/${ARCH}/configs/${config_name} ]]; then
