0a9b1d164daadf0b747c15f04135e41a9cffa263
Dezhen Wang <dezhen.wang@amlogic.com>
2024-10-21 05:59:38 +0000

    scripts: 6.6 mv common_drivers to up dir fix bug [1/1]
    PD#SWPL-189685
    
    Problem:
    not change mk_smarthome32.sh
    
    Solution:
    change path in mk_smarthome32.sh
    
    Verify:
    local
    
    Change-Id: I4ef61d4224e91fd2cf166b980b6aa112b927ae42
    Signed-off-by: Dezhen Wang <dezhen.wang@amlogic.com>
    (cherry picked from commit 4e3efd7be9db7da1260a6b25b324e193e1043fea)

--- scripts/amlogic/mk_smarthome32.sh
+++ scripts/amlogic/mk_smarthome32.sh
@@ -9,4 +9,4 @@ ROOT_DIR=`pwd`
 ARCH=arm
 DEFCONFIG=meson64_a32_smarthome_defconfig
 
-source ${ROOT_DIR}/common/common_drivers/scripts/amlogic/mk_smarthome_common.sh $@
+source ${ROOT_DIR}/common_drivers/scripts/amlogic/mk_smarthome_common.sh $@
