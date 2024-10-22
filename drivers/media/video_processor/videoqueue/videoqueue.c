78b58f9488ac5ffa0fb4d075eeb62e565f12eaff
qiyao.zhou <qiyao.zhou@amlogic.com>
2024-10-22 17:21:36 +0800

    videoqueue: video play black when parent control [1/1]
    PD#SWPL-187355
    
    Problem:
    Not get buf from VT when disconnect.
    
    Solution:
    Add get buf flow in VQ.
    
    Verify:
    txhd2
    
    Change-Id: I999091809493c9859672b21502fce057016568b3
    Signed-off-by: qiyao.zhou <qiyao.zhou@amlogic.com>

--- drivers/media/video_processor/videoqueue/videoqueue.c
+++ drivers/media/video_processor/videoqueue/videoqueue.c
@@ -843,6 +843,19 @@ static int do_file_thread(struct video_queue_dev *dev)
 			vq_print(dev->inst, P_OTHER, "no consumer\n");
 
 		if (dev->di_backend_en) {
+			while (1) {
+				ret = vt_dequeue_buffer(dev->dev_session, dev->tunnel_id,
+					&free_file, &fence_file);
+				if (ret) {
+					vq_print(dev->inst, P_OTHER, "dequeue all buf.\n");
+					dev->queue_count = 0;
+					dev->dq_count = 0;
+					break;
+				}
+				ret  = buf_mgr_q_checkin(dev->dp_buf_mgr, free_file);
+				if (ret)
+					vq_print(dev->inst, P_ERROR, "q_checkin vt fail.\n");
+			}
 			ret = buf_mgr_q_checkin(dev->dp_buf_mgr, ready_file);
 			if (ret)
 				vq_print(dev->inst, P_ERROR, "q_checkin fail.\n");
