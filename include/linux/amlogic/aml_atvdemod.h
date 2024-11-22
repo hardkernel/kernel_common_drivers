ef90a4d9a7bec97f7e29a7f5c8cb58b2e6bdae16
Huiwen Yu <huiwen.yu@amlogic.com>
2024-11-21 18:54:48 -0800

    atv_demod: Revert V4.02 commit (V4.03) [1/2]
    PD#SWPL-193956
    
    Problem:
    Revert "add ops callbacks mutex and state callbacks for tvafe (V4.02)".
    
    Solution:
    Revert "add ops callbacks mutex and state callbacks for tvafe (V4.02)"
    AMLATVDEMOD_VER "V4.03".
    
    Verify:
    T5M/TXHD2.
    
    This reverts commit 856293324e9ca77e91097010642bf788b3c80e44.
    
    Change-Id: I3d90b96bfce0d1033c3518ab86997b9e28450d17
    Signed-off-by: huiwen.yu <huiwen.yu@amlogic.com>

--- include/linux/amlogic/aml_atvdemod.h
+++ include/linux/amlogic/aml_atvdemod.h
@@ -16,20 +16,12 @@ typedef int (*hook_func1_t)(bool);
 /* For audio driver get atv audio state */
 void aml_fe_get_atvaudio_state(int *state);
 
-/* For audio driver get atv demod lock state */
-void aml_fe_get_atvdemod_state(int *state);
-
 /* For atv demod hook tvafe driver state */
 void aml_fe_hook_cvd(hook_func_t atv_mode, hook_func_t cvd_hv_lock,
 		hook_func_t get_fmt, hook_func1_t set_mode,
 		hook_func_t force_fmt);
 #else
 static inline __maybe_unused void aml_fe_get_atvaudio_state(int *state)
-{
-	*state = 0;
-}
-
-static inline __maybe_unused void aml_fe_get_atvdemod_state(int *state)
 {
 	*state = 0;
 }
