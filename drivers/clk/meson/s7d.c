21f451b6a969a283bd1dbd08274b7932022538ed
yiting.deng <yiting.deng@amlogic.com>
2025-01-08 15:41:31 +0800

    clk: s7d: fix k6.12 sys gate config [1/1]
    PD#SWPL-199318
    
    Problem:
    fix s7d kernel6.12 sys_clk gate error config
    
    Solution:
    fix it
    
    Verify:
    s7d
    
    Change-Id: Iae296a38d80332066cb3269d07c9bf5a88cd0f66
    Signed-off-by: yiting.deng <yiting.deng@amlogic.com>

--- drivers/clk/meson/s7d.c
+++ drivers/clk/meson/s7d.c
@@ -3825,10 +3825,28 @@ MESON_CLK_GATE_SYS_CLK(sys_ge2d, CLKCTRL_SYS_CLK_EN0_REG1, 20);
 MESON_CLK_GATE_SYS_CLK(sys_spicc0, CLKCTRL_SYS_CLK_EN0_REG1, 21);
 MESON_CLK_GATE_SYS_CLK(sys_usb, CLKCTRL_SYS_CLK_EN0_REG1, 26);
 MESON_CLK_GATE_SYS_CLK(sys_i2c_m_a, CLKCTRL_SYS_CLK_EN0_REG1, 30);
-MESON_CLK_GATE_SYS_CLK(sys_i2c_m_b, CLKCTRL_SYS_CLK_EN0_REG1, 31);
-MESON_CLK_GATE_SYS_CLK(sys_i2c_m_c, CLKCTRL_SYS_CLK_EN0_REG2, 0);
-MESON_CLK_GATE_SYS_CLK(sys_i2c_m_d, CLKCTRL_SYS_CLK_EN0_REG2, 1);
-MESON_CLK_GATE_SYS_CLK(sys_i2c_m_e, CLKCTRL_SYS_CLK_EN0_REG2, 2);
+
+#define MESON_CLK_GATE_HW(_name, _reg, _bit, _parent_hw)              \
+struct clk_regmap _name = {                                           \
+	.data = &(struct clk_regmap_gate_data) {                      \
+		.offset = (_reg),                                     \
+		.bit_idx = (_bit),                                    \
+	},                                                            \
+	.hw.init = &(struct clk_init_data) {                          \
+		.name = #_name,                                       \
+		.ops = &clk_regmap_gate_ops,                          \
+		.parent_hws = (const struct clk_hw *[]) {             \
+			&(_parent_hw)                                 \
+		},                                                    \
+		.num_parents = 1,                                     \
+		.flags = CLK_IGNORE_UNUSED,                           \
+	},                                                            \
+}
+
+MESON_CLK_GATE_HW(sys_i2c_m_b, CLKCTRL_SYS_CLK_EN0_REG1, 31, sys_i2c_m_a.hw);
+MESON_CLK_GATE_HW(sys_i2c_m_c, CLKCTRL_SYS_CLK_EN0_REG2, 0, sys_i2c_m_a.hw);
+MESON_CLK_GATE_HW(sys_i2c_m_d, CLKCTRL_SYS_CLK_EN0_REG2, 1, sys_i2c_m_a.hw);
+MESON_CLK_GATE_HW(sys_i2c_m_e, CLKCTRL_SYS_CLK_EN0_REG2, 2, sys_i2c_m_a.hw);
 MESON_CLK_GATE_SYS_CLK(sys_hdmitx_apb, CLKCTRL_SYS_CLK_EN0_REG2, 4);
 MESON_CLK_GATE_SYS_CLK(sys_i2c_s_a, CLKCTRL_SYS_CLK_EN0_REG2, 5);
 MESON_CLK_GATE_SYS_CLK(sys_hdmi20_aes, CLKCTRL_SYS_CLK_EN0_REG2, 9);
@@ -3836,16 +3854,18 @@ MESON_CLK_GATE_SYS_CLK(sys_mmc_apb, CLKCTRL_SYS_CLK_EN0_REG2, 11);
 MESON_CLK_GATE_SYS_CLK(sys_cpu_apb, CLKCTRL_SYS_CLK_EN0_REG2, 19);
 MESON_CLK_GATE_SYS_CLK(sys_vpu_intr, CLKCTRL_SYS_CLK_EN0_REG2, 25);
 MESON_CLK_GATE_SYS_CLK(sys_sar_adc, CLKCTRL_SYS_CLK_EN0_REG2, 28);
-MESON_CLK_GATE_SYS_CLK(sys_pwm_j, CLKCTRL_SYS_CLK_EN0_REG2, 29);
+
+struct clk_regmap sys_pwm_a;
+MESON_CLK_GATE_HW(sys_pwm_j, CLKCTRL_SYS_CLK_EN0_REG2, 29, sys_pwm_a.hw);
 MESON_CLK_GATE_SYS_CLK(sys_gic, CLKCTRL_SYS_CLK_EN0_REG2, 30);
-MESON_CLK_GATE_SYS_CLK(sys_pwm_i, CLKCTRL_SYS_CLK_EN0_REG2, 31);
-MESON_CLK_GATE_SYS_CLK(sys_pwm_h, CLKCTRL_SYS_CLK_EN0_REG3, 0);
-MESON_CLK_GATE_SYS_CLK(sys_pwm_g, CLKCTRL_SYS_CLK_EN0_REG3, 1);
-MESON_CLK_GATE_SYS_CLK(sys_pwm_f, CLKCTRL_SYS_CLK_EN0_REG3, 2);
-MESON_CLK_GATE_SYS_CLK(sys_pwm_e, CLKCTRL_SYS_CLK_EN0_REG3, 3);
-MESON_CLK_GATE_SYS_CLK(sys_pwm_d, CLKCTRL_SYS_CLK_EN0_REG3, 4);
-MESON_CLK_GATE_SYS_CLK(sys_pwm_c, CLKCTRL_SYS_CLK_EN0_REG3, 5);
-MESON_CLK_GATE_SYS_CLK(sys_pwm_b, CLKCTRL_SYS_CLK_EN0_REG3, 6);
+MESON_CLK_GATE_HW(sys_pwm_i, CLKCTRL_SYS_CLK_EN0_REG2, 31, sys_pwm_a.hw);
+MESON_CLK_GATE_HW(sys_pwm_h, CLKCTRL_SYS_CLK_EN0_REG3, 0, sys_pwm_a.hw);
+MESON_CLK_GATE_HW(sys_pwm_g, CLKCTRL_SYS_CLK_EN0_REG3, 1, sys_pwm_a.hw);
+MESON_CLK_GATE_HW(sys_pwm_f, CLKCTRL_SYS_CLK_EN0_REG3, 2, sys_pwm_a.hw);
+MESON_CLK_GATE_HW(sys_pwm_e, CLKCTRL_SYS_CLK_EN0_REG3, 3, sys_pwm_a.hw);
+MESON_CLK_GATE_HW(sys_pwm_d, CLKCTRL_SYS_CLK_EN0_REG3, 4, sys_pwm_a.hw);
+MESON_CLK_GATE_HW(sys_pwm_c, CLKCTRL_SYS_CLK_EN0_REG3, 5, sys_pwm_a.hw);
+MESON_CLK_GATE_HW(sys_pwm_b, CLKCTRL_SYS_CLK_EN0_REG3, 6, sys_pwm_a.hw);
 MESON_CLK_GATE_SYS_CLK(sys_pwm_a, CLKCTRL_SYS_CLK_EN0_REG3, 7);
 
 #define MESON_CLK_GATE_AXI_CLK(_name, _reg, _bit)                     \
