998bd74e3dd89d56d1456b4d7ce5583cb8e958d9
zijie.lin <zijie.lin@amlogic.com>
2025-03-05 07:43:18 +0000

    bl: remove pwm constant api [1/1]
    PD#SWPL-178325
    
    Problem:
    new pwm driver not support constant api
    
    Solution:
    100% duty set dutycycle same as period
    0% duty set 0 dutycycle
    
    Verify:
    t5m
    
    Change-Id: I95abc71ca2986658d9b646206133d22cc1c89182
    Signed-off-by: zijie.lin <zijie.lin@amlogic.com>

--- drivers/media/vout/lcd/backlight/bl_pwm.c
+++ drivers/media/vout/lcd/backlight/bl_pwm.c
@@ -310,14 +310,6 @@ static void bl_set_pwm_normal(struct bl_pwm_config_s *bl_pwm,
 	if (lcd_debug_print_flag & LCD_DBG_PR_BL_ADV)
 		bl_pwm_normal_state_print(&bl_pwm->pwm_data.state);
 
-	if (out_level == 0xff) {
-		pwm_constant_disable(bl_pwm->pwm_data.meson,
-				     bl_pwm->pwm_data.meson_index);
-	} else {
-		/* pwm duty 100% or 0% special control */
-		pwm_constant_enable(bl_pwm->pwm_data.meson,
-				    bl_pwm->pwm_data.meson_index);
-	}
 	pwm_apply_atomic(bl_pwm->pwm_data.pwm, &bl_pwm->pwm_data.state);
 }
 
