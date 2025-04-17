815c0aa0facc921da7d29859052832a00cae8a2c
yuna.liu <yuna.liu@amlogic.com>
2025-04-17 10:36:20 +0000

    decoder: CF2 decoder identify and report dv. [2/2]
    PD#SWPL-206598
    
    Problem:
    support report dv flag.
    
    Solution:
    decoder support report dv fla.
    
    Verify:
    ohm
    
    Change-Id: I9ce5abaff597d964299e0f3404c255f5473d80be
    Signed-off-by: yuna.liu <yuna.liu@amlogic.com>

--- include/linux/amlogic/media/utils/amstream.h
+++ include/linux/amlogic/media/utils/amstream.h
@@ -110,6 +110,7 @@ enum VIDEO_DEC_TYPE {
 #define DECODER_FATAL_ERROR_NO_MEM              (0x400 << 16)
 #define DECODER_FATAL_ERROR_UNSUPPORT_FORMAT    (0x800 << 16)
 #define DECODER_ES_INPUT_UNDERRUN               (0x2000 << 16)
+#define DECODER_REPORT_DV_FLAG                  (0x4000 << 16)
 
 #define DECODER_ERROR_MASK	(0xffff << 16)
 /* The total slot number for fifo_buf */
