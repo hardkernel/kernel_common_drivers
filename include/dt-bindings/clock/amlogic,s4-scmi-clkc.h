2ba08f6ba8adb0ce50fb83b25786a0158ca01b38
yiting.deng <yiting.deng@amlogic.com>
2025-06-17 16:04:03 +0800

    clk: s4: clk update to k6.12 [2/2]
    PD#SWPL-217839
    
    Problem:
    s4 clk update to k6.12
    
    Solution:
    fix it
    
    Verify:
    s4_ap222
    
    Change-Id: If1128b874cd73381c42b50669aabce8a8101aa81
    Signed-off-by: yiting.deng <yiting.deng@amlogic.com>

---
+++ include/dt-bindings/clock/amlogic,s4-scmi-clkc.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_AMLOGIC_S4_SCMI_CLKC_H
+#define _DT_BINDINGS_CLOCK_AMLOGIC_S4_SCMI_CLKC_H
+
+/*
+ * CLKID index values
+ */
+#define CLKID_SYS_CLK                          0
+#define CLKID_AXI_CLK                          1
+#define CLKID_CPU_CLK                          2
+#define CLKID_FIXED_PLL_DCO                    3
+#define CLKID_FIXED_PLL                        4
+#define CLKID_GP1_PLL                          5
+#define CLKID_ACLKM                            6
+#define CLKID_SYS_PLL_DIV16                    7
+#define CLKID_CPU_CLK_DIV16                    8
+#define CLKID_FCLK_50M_DIV                     9
+#define CLKID_FCLK_50M                         10
+
+#endif /* _DT_BINDINGS_CLOCK_AMLOGIC_S4_SCMI_CLKC_H */
