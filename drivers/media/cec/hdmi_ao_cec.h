2e4d2992ffcf237d876727f1ce8cf497397a755d
bowen cheng <bowen.cheng@amlogic.com>
2025-06-19 19:08:55 +0800

    cec: remove recv msg uevent [1/1]
    PD#SWPL-219005
    PD#SWPL-219186
    
    Problem:
    suspend interrupted by rx msg uevent
    
    Solution:
    cec framework don't use rx msg uevent
    
    Verify:
    t6w
    
    Change-Id: I8dc8c1c48b5f08f017bd075f67221336cab43949
    Signed-off-by: bowen cheng <bowen.cheng@amlogic.com>

--- drivers/media/cec/hdmi_ao_cec.h
+++ drivers/media/cec/hdmi_ao_cec.h
@@ -9,7 +9,7 @@
 #include <linux/clk.h>
 #include "hdmi_tx_cec_20.h"
 
-#define CEC_DRIVER_VERSION     "2024/09/30: cec support std hibernate"
+#define CEC_DRIVER_VERSION     "2025/06/26: remove rx msg uevent"
 
 #define CEC_DEV_NAME		"cec"
 
