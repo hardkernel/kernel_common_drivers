aea77ff9cb28fdb62aae3601f1f8da4f90b60c9c
Pengcheng Chen <pengcheng.chen@amlogic.com>
2025-07-23 16:05:21 +0800

    vpp: fix NV12/21 bandwidth hight issue [1/1]
    PD#SWPL-215860
    
    Problem:
    NV12/21 bandwidth test data high
    
    Solution:
    set burst len to 2 when no block mode
    
    Verify:
    t7c
    
    Change-Id: I5c33e4e734d085a6fff263ac04b85d18186c2ae4
    Signed-off-by: Pengcheng Chen <pengcheng.chen@amlogic.com>

--- drivers/media/video_sink/video_hw_s5.c
+++ drivers/media/video_sink/video_hw_s5.c
@@ -1,19 +1,6 @@
 // SPDX-License-Identifier: (GPL-2.0+ OR MIT)
 /*
- * drivers/amlogic/media/video_sink/video_hw_s5.c
- *
- * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
+ * Copyright (c) 2025 Amlogic, Inc. All rights reserved.
  */
 
 #include <linux/version.h>
@@ -6284,7 +6271,7 @@ static void vd_set_blk_mode_slice_s5(struct video_layer_s *layer, u32 slice, u8 
 	struct vd_mif_reg_s *vd_mif_reg = NULL;
 	struct vd_mif_linear_reg_s *vd_mif_linear_reg = NULL;
 
-	u32 pic_32byte_aligned = 0;
+	u32 pic_32byte_aligned = 0, burst_len = 2;
 	u8 vpp_index;
 
 	if (slice > SLICE_NUM)
@@ -6293,12 +6280,14 @@ static void vd_set_blk_mode_slice_s5(struct video_layer_s *layer, u32 slice, u8 
 	vd_mif_reg = &vd_proc_reg.vd_mif_reg[slice];
 	vd_mif_linear_reg = &vd_proc_reg.vd_mif_linear_reg[slice];
 	vpp_index = layer->vpp_index;
-	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
-		block_mode, 12, 2);
-	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
-		block_mode, 14, 2);
-	if (block_mode)
+	if (block_mode == 1 || block_mode == 2) {
+		burst_len = block_mode;
 		pic_32byte_aligned = 7;
+	}
+	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
+		burst_len, 12, 2);
+	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
+		burst_len, 14, 2);
 	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
 		(pic_32byte_aligned << 6) |
 		(block_mode << 4) |
@@ -6324,7 +6313,7 @@ void vd_set_blk_mode_s5(struct video_layer_s *layer, u8 block_mode)
 	struct vd_mif_reg_s *vd_mif_reg = NULL;
 	struct vd_mif_linear_reg_s *vd_mif_linear_reg = NULL;
 	u32 layer_index = 0;
-	u32 pic_32byte_aligned = 0;
+	u32 pic_32byte_aligned = 0, burst_len = 2;
 	u8 vpp_index;
 
 	if (layer->layer_id >= MAX_VD_CHAN_S5)
@@ -6337,12 +6326,14 @@ void vd_set_blk_mode_s5(struct video_layer_s *layer, u8 block_mode)
 	vd_mif_reg = &vd_proc_reg.vd_mif_reg[layer_index];
 	vd_mif_linear_reg = &vd_proc_reg.vd_mif_linear_reg[layer_index];
 	vpp_index = layer->vpp_index;
-	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
-		block_mode, 12, 2);
-	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
-		block_mode, 14, 2);
-	if (block_mode)
+	if (block_mode == 1 || block_mode == 2) {
+		burst_len = block_mode;
 		pic_32byte_aligned = 7;
+	}
+	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
+		burst_len, 12, 2);
+	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
+		burst_len, 14, 2);
 	cur_dev->rdma_func[vpp_index].rdma_wr_bits(vd_mif_reg->vd_if0_gen_reg3,
 		(pic_32byte_aligned << 6) |
 		(block_mode << 4) |
