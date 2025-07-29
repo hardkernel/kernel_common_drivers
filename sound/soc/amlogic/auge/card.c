adac94cdecc47bc7375c0c964fdd4ffd6e31b900
hui.liu <hui.liu@amlogic.com>
2025-07-29 07:25:27 +0000

    audio: Supported format for Audio HAL Format is missing. [1/1]
    PD#SWPL-225635
    
    Problem:
    1.The Audio HAL Format in the driver is missing several
    display formats.
    
    Solution:
    1.The audio_hal_format is consistent with audio_hal.
    All formats can be displayed normally through tinymix
    contents.
    
    Verify:
    yocto
    
    Change-Id: Idee4cdf477d557bee871dabc8ddd0d7dee18dfbd
    Signed-off-by: hui.liu <hui.liu@amlogic.com>

--- sound/soc/amlogic/auge/card.c
+++ sound/soc/amlogic/auge/card.c
@@ -66,6 +66,11 @@ static const char * const audio_format[] = {
 	"DOLBY_THD_ATMOS_PROMPT_ON_ATMOS",
 	"DOLBY_MAT_ATMOS_PROMPT_ON_ATMOS",
 	"DOLBY_AC4_ATMOS_PROMPT_ON_ATMOS",
+	"AAC",
+	"HEAAC",
+	"DTSX",
+	"MPEGH",
+	"MPEGH_ASI_UPDATE",
 };
 
 #ifdef CONFIG_AMLOGIC_AUDIO_CUT
@@ -84,6 +89,7 @@ static const char * const audio_output[] = {
 };
 #endif
 
+/*the same as "AUDIO HAL FORMAT" in audiohal*/
 enum audio_hal_format {
 	TYPE_PCM = 0,
 	TYPE_DTS_EXPRESS = 1,
@@ -106,6 +112,11 @@ enum audio_hal_format {
 	TYPE_TRUE_HD_ATMOS_PROMPT_ON_ATMOS = 18,
 	TYPE_MAT_ATMOS_PROMPT_ON_ATMOS = 19,
 	TYPE_AC4_ATMOS_PROMPT_ON_ATMOS = 20,
+	TYPE_AAC  = 21,
+	TYPE_HEAAC = 22,
+	TYPE_DTSX = 23,
+	TYPE_MPEGH = 24,
+	TYPE_MPEGH_ASI_UPDATE = 25,
 };
 
 #ifdef CONFIG_AMLOGIC_AUDIO_CUT
