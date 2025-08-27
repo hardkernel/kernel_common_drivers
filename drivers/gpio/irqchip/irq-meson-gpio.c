125e497863af5772e45daa6d2506c5003fe09be2
Qianggui Song <qianggui.song@amlogic.com>
2025-08-27 15:55:48 +0800

    gpio: t6w: support gpio irq function [1/1]
    PD#SWPL-229420
    
    Problem:
    t6w gpio irq not works
    
    Solution:
    add t6w gpio irq data
    
    Verify:
    t6w_bs311
    
    Change-Id: Iafc00aa1c5b08b44a8e7b5b9c34443c77ab3ce9a
    Signed-off-by: Qianggui Song <qianggui.song@amlogic.com>

--- drivers/gpio/irqchip/irq-meson-gpio.c
+++ drivers/gpio/irqchip/irq-meson-gpio.c
@@ -314,6 +314,11 @@ static const struct meson_gpio_irq_params s6_params = {
 static const struct meson_gpio_irq_params t6d_params = {
 	INIT_MESON_SC2_COMMON_DATA(131)
 };
+
+static const struct meson_gpio_irq_params t6w_params = {
+	INIT_MESON_SC2_COMMON_DATA(142)
+};
+
 #endif // end of ifndef CONFIG_AMLOGIC_ZAPPER_CUT
 static const struct meson_gpio_irq_params s1a_params = {
 	INIT_MESON_SC2_COMMON_DATA(40)
@@ -369,6 +374,7 @@ static const struct of_device_id meson_irq_gpio_matches[] = {
 	{ .compatible = "amlogic,meson-s7-gpio-intc", .data = &s7_params },
 	{ .compatible = "amlogic,meson-s6-gpio-intc", .data = &s6_params },
 	{ .compatible = "amlogic,meson-t6d-gpio-intc", .data = &t6d_params },
+	{ .compatible = "amlogic,meson-t6w-gpio-intc", .data = &t6w_params },
 #endif // end of ifndef CONFIG_AMLOGIC_ZAPPER_CUT
 	{ .compatible = "amlogic,meson-s1a-gpio-intc", .data = &s1a_params },
 	{ .compatible = "amlogic,meson-a4-gpio-intc", .data = &a4_params },
