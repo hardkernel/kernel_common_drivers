6993e8a13dda90b859317285f80e1152e93e625f
mingxiu.sun <mingxiu.sun@amlogic.com>
2025-11-26 17:46:17 +0800

    vdin: dv 420 8 need bypass_tunnel [1/1]
    PD#SWPL-244445
    
    Problem:
    For the 420 8-bit source, vdin did not configure the bypass_tunnel,
    resulting in the tunnel-detunnel process.
    
    Solution:
    VDIN configuration for bypass_tunnel
    
    Verify:
    t6w
    
    Change-Id: I056fa6be73d3c19c107c17557476caa124a6155f
    Signed-off-by: mingxiu.sun <mingxiu.sun@amlogic.com>

--- drivers/media/vin/tvin/vdin/vdin_hdr.c
+++ drivers/media/vin/tvin/vdin/vdin_hdr.c
@@ -326,9 +326,9 @@ bool vdin_dv_is_not_std_source_led(struct vdin_dev_s *devp)
 			else
 				devp->bypass_tunnel = true;
 		} else {
-			if ((devp->prop.color_format == TVIN_YUV420 ||
-				devp->prop.color_format == TVIN_RGB444) &&
-				devp->prop.colordepth == VDIN_COLOR_DEEPS_12BIT)
+			if (devp->prop.color_format == TVIN_YUV420 ||
+				(devp->prop.color_format == TVIN_RGB444 &&
+				devp->prop.colordepth == VDIN_COLOR_DEEPS_12BIT))
 				/* dv 420 12bit/rgb 12bit: recognize as dv but drop 2-lsb */
 				devp->bypass_tunnel = true;
 		}
